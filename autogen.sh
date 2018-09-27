2d7d5c0ebf53c064a57b233fb6dcfd21b678f9e4
tihmstar <tihmstar@gmail.com>
2018-09-27 13:31:26 +0200

    Revert "Make your shit script work"
    This reverts commit 4feddd9d30993d22ace0b5bb2bf0903edf2c84f0.

--- autogen.sh
+++ autogen.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-set -e;
-
-gprefix=`which glibtoolize 2>&1 >/dev/null`
-if [ $? -eq 0 ]; then 
-  glibtoolize --force
-else
-  libtoolize --force
-fi
-aclocal -I m4
+aclocal
+autoconf
 autoheader
 automake --add-missing
-autoconf
-
-if [ -z "$NOCONFIGURE" ]; then
-    ./configure "$@"
-fi
+autoreconf -i
+./configure "$@"
